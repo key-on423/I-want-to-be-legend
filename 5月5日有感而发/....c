385ec834d84f955a9f989b5cb9cdb3e34f1da36a
key-on423 <3531697760@qq.com>
2024-05-05 23:34:32 +0800

    I want to save every beauty in this world

---
+++ 5月5日有感而发/....c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("I want to save this world, save myself, save every beauty in this world....");
+	printf("\n\n\n                                                                   2024/5/5");
+}
